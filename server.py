34c7c6eb0a0e3edebb1832fcb5a7520555ad0cee
VeraBasko <vr.basko@gmail.com>
2020-05-24 21:04:32 +0500

    Работа с сентри

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
+import sentry_sdk
 import os
 from bottle import Bottle, request
-import sentry_sdk
 from sentry_sdk.integrations.bottle import BottleIntegration
 
-sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegration()])
-
+sentry_sdk.init(
+    dsn="https://615e9144b90f4db3bde501864352c1eb@o397304.ingest.sentry.io/5251658",
+    integrations=[BottleIntegration()]
+)
 app = Bottle()
 
-@app.route('/success')
-def success():
+@app.route('/fail')
+def index():
+    raise RuntimeError("There is an error!")
     return
 
-@app.route('/fail')
-def fail():
-    raise RuntimeError("Ошибка!!!")
-
+@app.route('/success')
+def index():
+    return
 
 app.run(host='localhost', port=8080)
+
+if os.environ.get("APP_LOCATION") == "heroku":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    app.run(host="localhost", port=8080, debug=True)
